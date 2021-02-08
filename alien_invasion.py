98eaf6bd2e346ea36b77f75150a159c6204296ae
lect <litaniasr@gmail.com>
2021-02-09 00:45:10 +0300

    Created module "settings.py" and refactored AlienInvasion class by moving settings into Settings class

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     """
@@ -8,24 +10,21 @@ class AlienInvasion:
     """
 
     def __init__(self):
-        """
-        Initializes game and creates game resources.
-        """
+        """Initializes game and creates game resources."""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (25, 25, 112)
 
     def run_game(self):
-        """
-        Launches main game cycle.
-        """
+        """Launches main game cycle."""
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
 
 
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """
+    Class holding all the settings of Alien Invasion game
+    """
+
+    def __init__(self):
+        """Initializes game settings"""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (25, 25, 112)
