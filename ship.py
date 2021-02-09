355d02928a4858508f0351dec3b3029f6667dca9
lect <litaniasr@gmail.com>
2021-02-09 14:09:18 +0300

    Added ship_speed settings

--- ship.py
+++ ship.py
@@ -10,10 +10,12 @@ class Ship:
         """Initializes the ship and sets its initial position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         self.image = pygame.image.load('images/space_ship.bmp')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
 
         self.moving_right = False
         self.moving_left = False
@@ -25,6 +27,7 @@ class Ship:
     def update(self):
         """Updates the ship's position according to flags."""
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
