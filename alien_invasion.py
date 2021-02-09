72289be4be18476c2c8a674458beb5f5964d9f53
lect <litaniasr@gmail.com>
2021-02-09 17:02:14 +0300

    Added max bullets limit. Refactored main game cycle by creating new method for updating bullets positions and deleting them. Aborted fullscreen mode.

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,9 +16,8 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                              self.settings.screen_height))
 
         pygame.display.set_caption("Alien Invasion")
         # Found typo in the book code (was undefined variable 'screen'
@@ -32,10 +31,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -70,8 +66,16 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Creates a new bullet and incorporates it into 'bullets' group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Updates bullets' positions and eliminates bullets out of screen."""
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """Refreshes screen."""
