4178c8f78758a60d81de3ad2bd62fc674e21981d
lect <litaniasr@gmail.com>
2021-02-09 19:54:07 +0300

    Added creation of 1st row of aliens in _create_fleet method of AI

--- alien_invasion.py
+++ alien_invasion.py
@@ -72,7 +72,14 @@ class AlienInvasion:
     def _create_fleet(self):
         """Creates alien intruders' fleet."""
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - 2 * alien_width
+        number_aliens_x = available_space_x // (2 * alien_width)
+        for alien_number in range(number_aliens_x):
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
     def _fire_bullet(self):
         """Creates a new bullet and incorporates it into 'bullets' group."""
