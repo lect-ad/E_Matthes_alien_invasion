5871ad834a28d4aa1a0be1cc2bb7804ca55736e3
lect <litaniasr@gmail.com>
2021-02-09 14:15:54 +0300

    Added limitations at screen edges in ship.update method

--- ship.py
+++ ship.py
@@ -26,8 +26,8 @@ class Ship:
 
     def update(self):
         """Updates the ship's position according to flags."""
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         self.rect.x = self.x
